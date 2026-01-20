6cdff97a3db0a8207d420fbd77063d1b96844731
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-01-20 09:42:01 +0700

    update build

--- public/build/assets/icon-DW6MbawF.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BhEHCJ0Z.js";import{c as t}from"./utils-CuQfd7NH.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
