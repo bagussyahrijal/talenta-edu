ff541e6c83d8355b666147de1c7b9b0d0085cd76
alifzaidan <zaidanalif22710@gmail.com>
2026-01-21 09:58:17 +0700

    update build

---
+++ public/build/assets/icon-XJCSIJv-.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Bo7nQjzy.js";import{c as t}from"./utils-D0WErUGI.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
---
+++ public/build/assets/index-BO-c_O2N.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-Bo7nQjzy.js";var t=a();const e=r(t);export{e as R,t as r};
