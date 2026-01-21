7bc27ca1eb36e86033b2b906fa339bc231e5780e
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-01-21 11:18:44 +0700

    update build

---
+++ public/build/assets/index-nAMbC8f_.js
@@ -0,0 +1,1 @@
+import{r as i,j as o}from"./app-DYAS-Qyq.js";import{P as t}from"./index-B66-Nk5f.js";var s="VisuallyHidden",a=i.forwardRef((r,e)=>o.jsx(t.span,{...r,ref:e,style:{position:"absolute",border:0,width:1,height:1,padding:0,margin:-1,overflow:"hidden",clip:"rect(0, 0, 0, 0)",whiteSpace:"nowrap",wordWrap:"normal",...r.style}}));a.displayName=s;var n=a;export{n as R,a as V};
