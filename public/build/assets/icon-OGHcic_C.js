bc2ae93e70cf0737815bfdf6648c81ef592f5813
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-02-18 09:14:06 +0700

    build

---
+++ public/build/assets/icon-OGHcic_C.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-PdC7gOEo.js";import{c as t}from"./utils-BV3CEx5d.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
