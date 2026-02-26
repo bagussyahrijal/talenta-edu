25ed749e9e2bd050ce0749e29d6c5d0020e4bb06
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-02-26 08:44:31 +0700

    ketinggalan 1

--- resources/js/pages/user/home/carousel-section.tsx
+++ resources/js/pages/user/home/carousel-section.tsx
@@ -20,7 +20,7 @@ export default function CarouselSection() {
                     <CarouselItem className="px-4">
                         <a href="#program-kami">
                             <img
-                                src="/assets/images/carousel-1.png"
+                                src="/assets/images/carousel-1.webp"
                                 alt="Slide 1"
                                 loading="lazy"
                                 className="mx-auto w-full max-w-7xl rounded-xl object-cover shadow-lg"
