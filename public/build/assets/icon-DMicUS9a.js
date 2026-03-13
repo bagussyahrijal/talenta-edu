0b0602a567abcb81083eed130242673c553af697
alifzaidan <zaidanalif22710@gmail.com>
2026-03-13 09:11:33 +0700

    update build

---
+++ public/build/assets/icon-DMicUS9a.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DH8XZrMy.js";import{c as t}from"./utils-DGwd7Ge2.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
