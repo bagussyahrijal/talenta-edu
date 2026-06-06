fe9382b3631e578c3bf6d83c5795ecd4605fbd95
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-06-06 09:24:51 +0700

    build

---
+++ public/build/assets/icon-DxmKfzzw.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B2rFQLXN.js";import{c as t}from"./utils-CbfMBVAY.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
