0e27f9debdf98bbd8846913b107d4bbdb71c7a05
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-06-24 11:13:49 +0700

    update build

--- public/build/assets/index-x3RtI2kB.js
+++
@@ -1,1 +0,0 @@
-import{g as r,a}from"./app-Dk1ltEYg.js";var t=a();const e=r(t);export{e as R,t as r};
---
+++ public/build/assets/use-motion-value-CRYG-AUn.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-C5pSnD2s.js";import{u as a,M as u,h as r}from"./proxy-BA11Yu7i.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
