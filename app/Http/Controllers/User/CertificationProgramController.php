e6ee51cccf39fe5310eef492122d8441f4ca8ddd
alifzaidan <zaidanalif22710@gmail.com>
2026-07-01 15:06:10 +0700

    npm run build

--- app/Http/Controllers/User/CertificationProgramController.php
+++ app/Http/Controllers/User/CertificationProgramController.php
@@ -18,8 +18,8 @@ class CertificationProgramController extends Controller
 {
     use WablasTrait;
 
-    private const ADMIN_WHATSAPP_URL = 'https://wa.me/+6285142505794';
-    private const ADMIN_WHATSAPP_NUMBER = '6285142505794';
+    private const ADMIN_WHATSAPP_URL = 'https://wa.me/+6285606391730';
+    private const ADMIN_WHATSAPP_NUMBER = '6285606391730';
 
     public function index()
     {
---
+++ public/build/assets/use-motion-value-BlVfsuse.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-DJt3eBNK.js";import{u as a,M as u,h as r}from"./proxy-DK9ABEQZ.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
