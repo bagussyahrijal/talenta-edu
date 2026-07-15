94abfc751cba61d097ddb8078ee4e924c552a22b
alifzaidan <zaidanalif22710@gmail.com>
2026-07-15 13:45:30 +0700

    update build

--- public/build/assets/icon-CTi08U_N.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BAt5-Ov3.js";import{c as t}from"./utils-Dtl_HzQ7.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
