d908ff77c2e699ab0d7ca566a78d1775e55ef64a
alifzaidan <zaidanalif22710@gmail.com>
2026-07-20 09:20:06 +0700

    update build

--- public/build/assets/icon-BNzJ9yXa.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DpBmU8h9.js";import{c as t}from"./utils-CfL_WG_w.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
