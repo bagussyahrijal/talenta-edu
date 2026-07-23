2981f1168e5e19959897a12dde407e3f018eb2ad
alifzaidan <zaidanalif22710@gmail.com>
2026-07-23 13:44:37 +0700

    update build

---
+++ public/build/assets/icon-CLyEUVf5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CbwBMZ6t.js";import{c as t}from"./utils-D-lpvRNB.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
