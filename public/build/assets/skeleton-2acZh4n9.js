a1aead07af80b0364ec77b492c16ad7abf5d08f8
alifzaidan <zaidanalif22710@gmail.com>
2026-08-01 11:18:31 +0700

    feat: add mandatory admin fee to certification program registration and update pricing UI summary

--- public/build/assets/skeleton-2acZh4n9.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BRE9RnJT.js";import{c as r}from"./utils-Jdqz54Vu.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
