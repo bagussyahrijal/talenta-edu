6b5b374c8307c33d3412372580227ed8fbbc4b7c
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-04 09:41:10 +0700

    update build

---
+++ public/build/assets/skeleton-BCaHisQc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CEkgA7sL.js";import{c as r}from"./utils-BbJ9ajG8.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
