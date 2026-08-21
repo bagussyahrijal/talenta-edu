0221227fbe39367f80b2064320ec6382b761bf4c
alifzaidan <zaidanalif22710@gmail.com>
2026-08-21 13:09:27 +0700

    update build

---
+++ public/build/assets/index-CqfX0oWc.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-BfQeOYr5.js";var t=a();const e=r(t);export{e as R,t as r};
---
+++ public/build/assets/skeleton-Cu3Q7Sl0.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BfQeOYr5.js";import{c as r}from"./utils-BYj_A2GR.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
