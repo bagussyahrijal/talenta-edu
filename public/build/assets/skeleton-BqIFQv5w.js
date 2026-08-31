68959604301786bc0d57b19dea107087cf2653e0
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-08-31 09:46:23 +0700

    build

---
+++ public/build/assets/skeleton-BqIFQv5w.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CJwEYvT3.js";import{c as r}from"./utils-D8EqqwB6.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
